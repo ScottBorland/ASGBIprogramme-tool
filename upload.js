47d21655bc1f9090ac96267d89717d79975cf261
ScottBorland <scottborland0@gmail.com>
2022-10-06 20:52:11 +0100

    Change post request url to fly server

--- upload.js
+++ upload.js
@@ -1,6 +1,9 @@
+//Fly server: https://long-hill-1888.fly.dev
+//Heroku server: https://asgbi-programme.herokuapp.com 
+
 const upload = async (rows) =>{
   alert('Spreadsheet has been uploaded and is now viewable at https://scottborland.github.io/ASGBI-public-view')
-    fetch('https://asgbi-programme.herokuapp.com', {
+    fetch('https://long-hill-1888.fly.dev', {
     method: 'POST',
     mode: 'cors',
     headers: {
